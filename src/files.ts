9df16e8adbc327ded41bdefb65639bf28e4da60b
Maxime Moreillon <m.moreillon@gmail.com>
2024-06-18 12:42:13 +0900

    reading file

--- src/files.ts
+++ src/files.ts
@@ -1,16 +1,16 @@
 import { BucketItem, BucketStream } from "minio"
 import { minioClient, S3_BUCKET} from "./s3"
 
-const readStream = (dataStream: BucketStream<BucketItem>) =>
+export const readStream = (dataStream: BucketStream<BucketItem>): Promise<any[]> =>
   new Promise((resolve, reject) => {
     const buffer: any[] = []
-    dataStream.on("data", (chunk: any) => {
+    dataStream.on("data", (chunk) => {
       buffer.push(chunk)
     })
     dataStream.on("end", () => {
       resolve(buffer)
     })
-    dataStream.on("error", (err: any) => {
+    dataStream.on("error", (err) => {
       reject(err)
     })
   })
@@ -19,12 +19,13 @@ const readStream = (dataStream: BucketStream<BucketItem>) =>
 export const storeFile = async (file: File) => {
 
   const filename = (file as File).name
-  const blob = file.toString()
 
-  await minioClient.putObject(S3_BUCKET, filename, blob)
+  // TODO: use a stream
+  const buffer = Buffer.from( await file.arrayBuffer())
+
+  await minioClient.putObject(S3_BUCKET, filename, buffer)
 }
 
-export const readFiles = async () => {
-  const dataStream = await minioClient.listObjectsV2(S3_BUCKET)
-  return await readStream(dataStream)
-}
+export const readFiles = () => minioClient.listObjectsV2(S3_BUCKET).toArray()
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { stream } from 'hono/streaming'
 
 import {minioClient, S3_BUCKET} from './s3'
-import { readFiles, storeFile } from './files'
+import { readFile, readFiles, storeFile, readStream} from './files'
 
 const app = new Hono()
 
@@ -19,6 +20,16 @@ app.post('/files', async (c) => {
 app.get('/files', async (c) => {
   const files = await readFiles()
   return c.json({files})
+})
+
+
+app.get('/files/:key', async (c) => {
+  
+  // TODO: this is most likely not the right way
+  const {key} = c.req.param()
+  const dataStream = await minioClient.getObject(S3_BUCKET, key)
+  const buffer: any = await readStream(dataStream)
+  return c.newResponse(buffer)
 })
 
 
