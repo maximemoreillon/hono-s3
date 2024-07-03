79052c643749ea03b53a15e71f16abeadae91542
Maxime Moreillon <m.moreillon@gmail.com>
2024-07-03 10:43:57 +0900

    ReadableStream

--- src/files.ts
+++ src/files.ts
@@ -1,31 +1,12 @@
-import { BucketItem, BucketStream } from "minio"
-import { minioClient, S3_BUCKET} from "./s3"
-
-export const readStream = (dataStream: BucketStream<BucketItem>): Promise<any[]> =>
-  new Promise((resolve, reject) => {
-    const buffer: any[] = []
-    dataStream.on("data", (chunk) => {
-      buffer.push(chunk)
-    })
-    dataStream.on("end", () => {
-      resolve(buffer)
-    })
-    dataStream.on("error", (err) => {
-      reject(err)
-    })
-  })
-
+import { minioClient, S3_BUCKET } from "./s3"
 
 export const storeFile = async (file: File) => {
-
   const filename = (file as File).name
 
   // TODO: use a stream
-  const buffer = Buffer.from( await file.arrayBuffer())
+  const buffer = Buffer.from(await file.arrayBuffer())
 
   await minioClient.putObject(S3_BUCKET, filename, buffer)
 }
 
 export const readFiles = () => minioClient.listObjectsV2(S3_BUCKET).toArray()
-
-
--- src/index.ts
+++ src/index.ts
@@ -1,40 +1,49 @@
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import { stream } from 'hono/streaming'
-import {minioClient, S3_BUCKET} from './s3'
-import { readFiles, storeFile, readStream} from './files'
+import { Hono } from "hono"
+import { cors } from "hono/cors"
+import { minioClient, S3_BUCKET } from "./s3"
+import { readFiles, storeFile } from "./files"
 
 const app = new Hono()
 
 app.use(cors())
 
-app.post('/files', async (c) => {
-  const {file} = await c.req.parseBody()
-  
+app.post("/files", async (c) => {
+  const { file } = await c.req.parseBody()
+
   await storeFile(file as File)
 
-  return c.json({filename: (file as File).name})
+  return c.json({ filename: (file as File).name })
 })
 
-app.get('/files', async (c) => {
+app.get("/files", async (c) => {
   const files = await readFiles()
-  return c.json({files})
+  return c.json({ files })
 })
 
-
-app.get('/files/:key', async (c) => {
-  
+app.get("/files/:key", async (c) => {
   // TODO: this is most likely not the right way
-  const {key} = c.req.param()
-  const dataStream = await minioClient.getObject(S3_BUCKET, key)
-  const buffer: any = await readStream(dataStream)
-  return c.newResponse(buffer)
+  const { key } = c.req.param()
+  const stream = await minioClient.getObject(S3_BUCKET, key)
+
+  const readableStream = new ReadableStream({
+    start(controller) {
+      stream.on("data", (chunk) => {
+        controller.enqueue(chunk)
+      })
+      stream.on("end", () => {
+        controller.close()
+      })
+      stream.on("error", (err) => {
+        controller.error(err)
+      })
+    },
+  })
+
+  return c.newResponse(readableStream)
 })
 
-
-
-export default { 
-  port: 3000, 
-  host: '0.0.0.0',
-  fetch: app.fetch, 
-} 
+export default {
+  port: 3000,
+  host: "0.0.0.0",
+  fetch: app.fetch,
+}
